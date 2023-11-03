942d4aa0d828bb7ed4ef5511aad9b5d078de1a26
zoey-lee-korea <wngpsid4014@gmail.com>
2023-11-03 11:42:13 +0900

    feat : _app.js 공통영역 추가

---
+++ front/pages/_app.js
@@ -0,0 +1,21 @@
+import Head from 'next/head';
+import PropTypes from 'prop-types';
+import 'antd/dist/antd.css';
+
+const App = ({Component}) => {
+    return (
+        <>
+          <Head>
+            <meta charSet='utf-8'/>
+            <title>NodeBird</title>
+          </Head>
+          <Component />
+        </>
+    );
+};
+
+App.propTypes = {
+    Component : PropTypes.elementType.isRequired,
+}
+
+export default App;
