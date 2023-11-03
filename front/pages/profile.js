339a918cf488087130f73191c5bd32f35b941400
zoey-lee-korea <wngpsid4014@gmail.com>
2023-11-03 17:12:41 +0900

    섹션1-8. 프로필 페이지 제작

--- front/pages/profile.js
+++ front/pages/profile.js
@@ -1,14 +1,28 @@
 import Head from 'next/head';
 import AppLayout from '../components/AppLayout';
+import NicknameEditForm from '../components/NicknameEditForm';
+import FollowList from '../components/FollowList';
 
 const Profile = () => {
+    const followerList = [{ nickname: 'follower1' }, { nickname: 'follower2' }, { nickname: 'follower3' }];
+    const followingList = [{ nickname: 'following1' }, { nickname: 'following2' }, { nickname: 'following3' }];
     return (
-        <AppLayout>
+        <>
             <Head>
                 <title>내 프로필 | NodeBird</title>
             </Head>
-            <div>내 프로필</div>
-        </AppLayout>
+            <AppLayout>
+                <NicknameEditForm/>
+                <FollowList
+                    header="팔로잉 목록"
+                    data={followingList}
+                />
+                <FollowList
+                    header="팔로워 목록"
+                    data={followerList}
+                />
+            </AppLayout>
+        </>
     );
 };
 
