7a026b038eb0ae603d764647a01677150b2a5300
zoey-lee-korea <wngpsid4014@gmail.com>
2023-11-06 16:19:23 +0900

    feat: 게시글ImageZoom 기본 포맷 제작

---
+++ front/components/ImageZoom/index.js
@@ -0,0 +1,17 @@
+import React, { useState } from 'react';
+import PropTypes from 'prop-types';
+import Slick from 'react-slick';
+import { Overlay, Header, CloseBtn, SlickWrapper, ImgWrapper, Indicator, Global } from './styles';
+
+const ImagesZoom = ({ images, onClose }) => {
+    
+};
+
+ImagesZoom.propTypes = {
+  images: PropTypes.arrayOf(PropTypes.shape({
+    src: PropTypes.string,
+  })).isRequired,
+  onClose: PropTypes.func.isRequired,
+};
+
+export default ImagesZoom;
