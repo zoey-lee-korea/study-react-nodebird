18d7477367be08f3306f6f891739c795e2cbe749
zoey-lee-korea <wngpsid4014@gmail.com>
2023-11-06 16:13:49 +0900

    feat: 게시글Image 기본 포맷 제작

--- front/components/PostImages.js
+++ front/components/PostImages.js
@@ -5,8 +5,52 @@ import { PlusOutlined } from '@ant-design/icons';
 
 const PostImages = ({ images }) => {
 
+    const [showImagesZoom, setShowImagesZoom] = useState(false);
+    const onZoom = useCallback(() => {
+      setShowImagesZoom(true);
+    }, []);
+    const onClose = useCallback(() => {
+      setShowImagesZoom(false);
+    }, []);
+
+    // Image 1개
+    if (images.length === 1) {
+      return (
+        <>
+          <img role="presentation" src={images[0].src} alt={images[0].src} onClick={onZoom} />
+        </>
+      );
+    }
+
+    // Image 2개
+    if (images.length === 2) {
+        return (
+          <>
+            <div>
+              <img role="presentation" style={{ width: '50%', display: 'inline-block'}} src={images[0].src} alt={images[0].src}  onClick={onZoom} />
+              <img role="presentation" style={{ width: '50%', display: 'inline-block'}} src={images[1].src} alt={images[1].src} onClick={onZoom} />
+            </div>
+          </>
+        );
+      }
+
+    // Image 3개 이상
     return (
-        <div> Post Images</div>
+        <>
+          <div>
+            <img role="presentation" src={images[0].src} alt={images[0].src} width="50%" onClick={onZoom} />
+            <div
+              role="presentation"
+              style={{ display: 'inline-block', width: '50%', textAlign: 'center', verticalAlign: 'middle' }}
+              onClick={onZoom}
+            >
+              <PlusOutlined />
+              <br />
+              {images.length - 1}
+              개의 사진 더보기
+            </div>
+          </div>
+        </>
     )
 };
 
