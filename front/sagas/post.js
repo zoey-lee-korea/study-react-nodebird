ac7d1f56bfa50d97358ede7e9a1021e99af23835
zoey-lee-korea <wngpsid4014@gmail.com>
2023-11-07 11:46:57 +0900

    feat: saga 폴더구조 기본 세팅

---
+++ front/sagas/post.js
@@ -0,0 +1,9 @@
+import axios from 'axios';
+import shortId from 'shortid';
+import { all, delay, fork, put, takeLatest, throttle } from 'redux-saga/effects';
+
+
+export default function* postSaga() {
+    yield all([
+    ]);
+}
---
+++ front/sagas/user.js
@@ -0,0 +1,7 @@
+import { all, delay, fork, put, takeLatest } from 'redux-saga/effects';
+import axios from 'axios';
+
+export default function* userSaga() {
+    yield all([
+    ]);
+}
