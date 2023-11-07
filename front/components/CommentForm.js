6e6620a263c50b4a49a2a3e5000784c9ae1336f9
zoey-lee-korea <wngpsid4014@gmail.com>
2023-11-07 18:24:42 +0900

    feat: 댓글작성에 dispatch 적용

--- front/components/CommentForm.js
+++ front/components/CommentForm.js
@@ -1,25 +1,31 @@
 import { Button, Form, Input } from 'antd';
 import React, { useCallback, useState } from 'react';
 import PropTypes from 'prop-types';
+import { useDispatch, useSelector } from 'react-redux';
 
 const CommentForm = ({ post }) => {
-    const [commentText, setCommentText] = useState('');
-    const onChangeCommentText = useCallback((e) => {
-        setCommentText(e.target.value);
-      }, []);
-    
-    const onSubmitComment = useCallback(() => {
-      console.log(commentText);
-    }, [commentText]);
+  const [commentText, setCommentText] = useState('');
+  const onChangeCommentText = useCallback((e) => {
+      setCommentText(e.target.value);
+    }, []);
+  
+  const dispatch = useDispatch();
+  const onSubmitComment = useCallback(() => {
+    console.log(commentText);
+    dispatch({
+        type: ADD_COMMENT_REQUEST ,
+        data: {content: commentText, postId: post.id, userId: id}
+    })
+  }, [commentText, id]);
 
-    return (
-        <Form onFinish={onSubmitComment}>
-          <Form.Item style={{ position: 'relative', margin: 0 }}>
-            <Input.TextArea value={commentText} onChange={onChangeCommentText} />
-            <Button style={{ position: 'absolute', right: 0, bottom: -40 }} type="primary" htmlType="submit">댓글</Button>
-          </Form.Item>
-        </Form>
-    )
+  return (
+    <Form onFinish={onSubmitComment}>
+      <Form.Item style={{ position: 'relative', margin: 0 }}>
+        <Input.TextArea value={commentText} onChange={onChangeCommentText} />
+        <Button style={{ position: 'absolute', right: 0, bottom: -40 }} type="primary" htmlType="submit">댓글</Button>
+      </Form.Item>
+    </Form>
+  )
 }
 
 
