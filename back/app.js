132f2e0fd31d9ce63f625f61c646ee3fd933f270
zoey-lee-korea <wngpsid4014@gmail.com>
2023-11-10 10:21:33 +0900

    섹션4-7. Sequelize 기본 Sync 구현 (DB-app 연결)

--- back/app.js
+++ back/app.js
@@ -2,8 +2,16 @@ const express = require('express');
 
 const postRouter = require('./routes/post');
 const userRouter = require('./routes/user');
+const db = require('./models');
 
 const app = express();
+
+// 서버 실행 시 DB Sequelize 연결
+db.sequelize.sync()
+    .then(() => {
+        console.log('db 연결 성공');
+    })
+    .catch(console.error);
 
 app.get('/', (req, res) => {
     res.send('hello express');
--- back/models/index.js
+++ back/models/index.js
@@ -6,8 +6,18 @@ const db = {};
 
 const sequelize = new Sequelize(config.database, config.username, config.password, config);
 
+// 각 Model을 Sequelize에 등록
+db.Comment = require('./comment')(sequelize, Sequelize);
+db.Hashtag = require('./hashtag')(sequelize, Sequelize);
+db.Image = require('./image')(sequelize, Sequelize);
+db.Post = require('./post')(sequelize, Sequelize);
+db.User = require('./user')(sequelize, Sequelize);
+
+// 각 Model별 associate 부분을 찾아서 연결해주는 역할
 Object.keys(db).forEach(modelName => {
-  db[modelName].init(sequelize);
+  if (db[modelName].associate) {
+    db[modelName].associate(db);
+  }
 });
 
 db.sequelize = sequelize;
