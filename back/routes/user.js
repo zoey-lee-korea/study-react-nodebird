8fd525080584b13004ce6ac75c4a92bdc9a60287
zoey-lee-korea <wngpsid4014@gmail.com>
2023-11-10 17:18:33 +0900

    섹션4-14. 로그인 Middleware (isLoggedIn, isNotLoggedIn)

--- back/routes/user.js
+++ back/routes/user.js
@@ -3,10 +3,11 @@ const bcrypt = require('bcrypt');
 const passport = require('passport');
 
 const { User, Post } = require('../models');
+const { isLoggedIn, isNotLoggedIn } = require('./middlewares');
 
 const router = express.Router();
 
-router.post('/login', (req, res, next) => {
+router.post('/login', isNotLoggedIn, (req, res, next) => {
     passport.authenticate('local', (err, user, info) => { // POST /user/login (로그인)
         if (err) {
             console.error(err);
@@ -44,13 +45,13 @@ router.post('/login', (req, res, next) => {
     })(req, res, next);
 });
 
-router.post('/logout', (req, res) => {
+router.post('/logout', isLoggedIn, (req, res) => {
     req.logout();
     req.session.destroy();
     res.send('ok');
 });
 
-router.post('/', async (req, res, next) => { // POST /user (회원가입)
+router.post('/', isNotLoggedIn, async (req, res, next) => { // POST /user (회원가입)
     try {
         // 1.중복 ID 체크
         const exUser = await User.findOne({
