9f7a135b9740533fe238fe107b01079adfd23bea
zoey-lee-korea <wngpsid4014@gmail.com>
2023-11-10 15:10:49 +0900

    feat: dotenv 적용 (process.env.XXX)

--- back/config/config.js
+++ back/config/config.js
@@ -1,7 +1,10 @@
-{
+const dotenv = require('dotenv');
+dotenv.config();
+
+module.exports = {
   "development": {
     "username": "root",
-    "password": null,
+    "password": process.env.DB_PASSWORD,
     "database": "react-nodebird",
     "host": "127.0.0.1",
     "port": 3306,
